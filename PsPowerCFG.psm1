d1600fa6665ba65337fb965bdcc6a85f1f994091
Joseph Ascanio <joseph.ascanio@gmail.com>
2019-08-29 15:25:27 -0400

    Update PsPowerCFG.psm1
    added some error handling to Export-SRUMDB. If it works out I'm gonig to replicate it to the other functions.

--- PsPowerCFG.psm1
+++ PsPowerCFG.psm1
@@ -17,7 +17,7 @@ Function Test-IsAdmin {
     [CmdletBinding()]
     Param()
     Process {
-        $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+        $user = [Security.Principal.WindowsIdentity]::GetCurrent()
         (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
     }
 }
@@ -339,12 +339,37 @@ function Export-SRUMDB {
                 $OutFile = $OutFile.FullName -Replace "$($OutFile.Extension)", ".xml"
             }
 
-            Start-Process -FilePath powercfg.exe -ArgumentList "/SRUMUTIL /OUTPUT ""$($OutFile)"" /XML" -WindowStyle Hidden -Wait -RedirectStandardError "$($env:temp)\error.txt" -RedirectStandardOutput "$($env:temp)\out.txt"
-            $errorContent = Get-Content "$($env:temp)\error.txt"
-            $outputContent = Get-Content "$($env:temp)\out.txt"
+            $result = Start-Process -FilePath powercfg.exe -ArgumentList "/SRUMUTIL /OUTPUT ""$($OutFile)"" /XML" -WindowStyle Hidden -Wait -RedirectStandardError "$($env:temp)\error.txt" -RedirectStandardOutput "$($env:temp)\out.txt" -PassThru
 
-            Write-Output "StdErr: $($errorContent)"
-            Write-Output "StdOut: $($outputContent)"
+            if ($result -ne 0) {
+                Write-Verbose "Retrieving and resolving error message."
+                $errorContent = Get-Content "$($env:temp)\error.txt"
+
+                Write-Error -Message $errorContent
+                break
+            } else {
+                Write-Output "SRUM Database saved to file path: $($Outfile.FullName)"
+            }
+        }
+
+        If ($FormatCSV) {
+            Write-Verbose "Formatting Report as CSV"
+            If (!($OutFile.Extension -eq '.csv')) {
+                Write-Verbose "Converting $($OutFile.FullName) to csv"
+                $OutFile = $OutFile.FullName -Replace "$($OutFile.Extension)", ".csv"
+            }
+
+            $result = Start-Process -FilePath powercfg.exe -ArgumentList "/SRUMUTIL /OUTPUT ""$($OutFile)"" /CSV" -WindowStyle Hidden -Wait -RedirectStandardError "$($env:temp)\error.txt" -RedirectStandardOutput "$($env:temp)\out.txt" -PassThru
+
+            if ($result -ne 0) {
+                Write-Verbose "Retrieving and resolving error message."
+                $errorContent = Get-Content "$($env:temp)\error.txt"
+
+                Write-Error -Message $errorContent
+                break
+            } else {
+                Write-Output "SRUM Database saved to file path: $($Outfile.FullName)"
+            }
         }
     }
 }
